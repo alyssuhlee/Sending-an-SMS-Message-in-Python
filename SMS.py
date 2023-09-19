3bd791253a8e3914886806564a92e75ce20cf2b6
Alyssa Alexandra Lee <aslee2@up.edu.ph>
2023-09-19 17:36:57 +0800

    Initial commit

---
+++ SMS.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+account_sid = '[ACCOUNT SID HERE]'
+auth_token = '[AUTH TOKEN HERE]'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_='[TWILIO PHONE NUMBER]',
+  body = '[MESSAGE]',
+  to='[YOUR PHONE NUMBER]'
+)
+
+print(message.sid)
